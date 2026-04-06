8bc64f415152fe7204c0a369d7441b5c4e74cf9f
NathanielHawron <100041819+NathanielHawron@users.noreply.github.com>
2026-04-05 21:42:33 -0700

    Create tasks.h

---
+++ tasks.h
@@ -0,0 +1,25 @@
+// Blink built-in LED at a frequency
+void taskB(){
+  static int blinkDuration = 1000;
+
+}
+// Set the frequency of taskB using ICP
+void taskCB(){
+  
+}
+// Monitor all other tasks for period, duration, average actual duration, and failure/rejection counts
+void taskM(){
+
+}
+// Send taskM data over Serial using IPC, non-preemptable
+void taskS(){
+
+}
+// Compute math operations provided by Serial, non-preemptable
+void taskC(){
+
+}
+// Try to fail other tasks, using methods that work on naive task schedulers (100% utilization, short tasks, acquire unused resources indefinately, aqcuire random resources)
+void taskF(){
+
+}
