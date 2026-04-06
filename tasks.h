fed06963d78f556789b54c28da039502c5438b06
NathanielHawron <100041819+NathanielHawron@users.noreply.github.com>
2026-04-05 21:43:47 -0700

    Update tasks.h
    Moved implementation to tasks.c

--- tasks.h
+++ tasks.h
@@ -1,25 +1,12 @@
 // Blink built-in LED at a frequency
-void taskB(){
-  static int blinkDuration = 1000;
-
-}
+void taskB();
 // Set the frequency of taskB using ICP
-void taskCB(){
-  
-}
+void taskCB();
 // Monitor all other tasks for period, duration, average actual duration, and failure/rejection counts
-void taskM(){
-
-}
+void taskM();
 // Send taskM data over Serial using IPC, non-preemptable
-void taskS(){
-
-}
+void taskS();
 // Compute math operations provided by Serial, non-preemptable
-void taskC(){
-
-}
+void taskC();
 // Try to fail other tasks, using methods that work on naive task schedulers (100% utilization, short tasks, acquire unused resources indefinately, aqcuire random resources)
-void taskF(){
-
-}
+void taskF();
