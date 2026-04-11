1695787bb9514bad529c2b7dc1fd7710e9807015
Julian Joaquin <jnjoaquin@shaw.ca>
2026-04-10 17:31:54 -0700

    Fixed RT version error, added starting code for basic IPC

--- nuclear_chibios/main.c
+++ nuclear_chibios/main.c
@@ -3,15 +3,28 @@
 
 #include "tasks.h"
 
+#define BLINK_MAILBOX_SIZE 128U
+
+static msg_t mbBlink_buffer[BLINK_MAILBOX_SIZE];
+static mailbox_t mbBlink;
+
 static THD_WORKING_AREA(waThread_B, 128);
-static THD_FUNCTION(Thread_B, args) {
-  (void)args;
+static THD_WORKING_AREA(waThread_CB, 128);
+
+static THD_FUNCTION(Thread_B, arg) {
+  (void)arg;
   taskB();
+}
+
+static THD_FUNCTION(Thread_CB, arg) {
+  taskCB((mailbox_t *)arg);
 }
 
 int main() {
   halInit();
   chSysInit();
+
+  chMBObjectInit(&mbBlink, mbBlink_buffer, MAILBOX_SIZE);
 
   chThdCreateStatic(waThread_B, sizeof(waThread_B), NORMALPRIO, Thread_B, NULL);
 
